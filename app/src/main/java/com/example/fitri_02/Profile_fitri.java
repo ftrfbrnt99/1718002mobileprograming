bf6379462607627486a7412621129e4fe4137497
ftrfbrnt99 <ftrfbrnt99@gmail.com>
2019-10-15 16:03:10 +0700

    versi 1.0.0

---
+++ app/src/main/java/com/example/fitri_02/Profile_fitri.java
@@ -0,0 +1,15 @@
+package com.example.fitri_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Profile_fitri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_fitri);
+    }
+}
